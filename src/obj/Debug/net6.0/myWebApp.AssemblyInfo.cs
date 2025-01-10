bfc4705261dca011b46391941cda37ff4bd884a9
lythienhoang <lythienhoang@gmail.com>
2025-01-10 11:29:32 +0700

    Completed task-2.

--- src/obj/Debug/net6.0/myWebApp.AssemblyInfo.cs
+++ src/obj/Debug/net6.0/myWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("myWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1e693915b4787554d5424d3481cd75de941deeb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6621aa03310b24fb714f5150db126285cc5e5da2")]
 [assembly: System.Reflection.AssemblyProductAttribute("myWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("myWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
