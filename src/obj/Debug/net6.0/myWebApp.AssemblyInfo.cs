fd360273c6a4bf24687988f7bf18b12226f0d5a9
lythienhoang <lythienhoang@gmail.com>
2025-01-11 12:32:35 +0700

    Update all files in src diredtory along task-4 branch rebase task-3 branch.

--- src/obj/Debug/net6.0/myWebApp.AssemblyInfo.cs
+++ src/obj/Debug/net6.0/myWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("myWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6621aa03310b24fb714f5150db126285cc5e5da2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfc4705261dca011b46391941cda37ff4bd884a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("myWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("myWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
